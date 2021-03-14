105b972f3777148c21df5ced45e611535dd4bb5b
NeSoSiSka <78197152+NeSoSiSka@users.noreply.github.com>
2021-03-14 22:40:02 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let num = 0
+  for(i = 0; i < bracketsConfig.length; i++) {
+      if((((bracketsConfig[i] == '(') || (bracketsConfig[i] == '[' || (bracketsConfig[i] == '{') || (bracketsConfig[i] == '|')))))
+      {
+      num++
+      }
+      else{
+      if((bracketsConfig[i] == ')') || (bracketsConfig[i] == ']') || (bracketsConfig[i] == '}') || (bracketsConfig[i] == '|'))
+        {
+        num--
+        }
+      }
+    }
+    if(num == 0){
+      return str= true
+  }
+  else{
+    return str= false
+  }
 }
